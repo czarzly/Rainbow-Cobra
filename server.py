d959ac67b3952b7b73f5abf2c09d281f96c70604
eran <czarzly@gmail.com>
2016-06-21 21:57:01 +0300

    Added contact-us handler

--- server.py
+++ server.py
@@ -1,12 +1,20 @@
 #! /usr/bin/python
 
 from flask import Flask
+from flask import Response
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
+
+@app.route('/contact-us', methods=["POST"])
+def contact_us():
+    print(request.values["key"])
+    return Response("ok")
+
 
 if __name__ == "__main__":
      app.run()
