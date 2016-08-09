a1aa46e34defd83f6525b86c058b9f15342837d2
eran <czarzly@gmail.com>
2016-08-09 20:36:59 +0300

    Added Mongo

--- server.py
+++ server.py
@@ -3,8 +3,13 @@
 from flask import Flask, jsonify
 from flask import Response
 from flask import request
+from pymongo import MongoClient
+
 
 app = Flask(__name__)
+
+client = MongoClient()
+
 
 
 @app.route('/')
@@ -18,7 +23,13 @@ def contact():
 @app.route('/contact-us', methods=["POST"])
 def contact_us():
     print(request.values['key'])
-    return jsonify({"hi": request.values['key']})
+    contactinfo = {"hi": request.values['key']}
+    client.rainbow.contacts.insert(contactinfo)
+
+    contactinfo.pop('_id')
+    print(contactinfo)
+    return jsonify(contactinfo)
+
 
 if __name__ == "__main__":
      app.run()
